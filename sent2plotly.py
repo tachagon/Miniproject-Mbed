6a1b71be1762eb73c39c8a90fa6f211296d81b1e
tachagon <o_k_t@hotmail.com>
2014-11-11 17:36:12 +0700

    edit plotly is streaming data to cloud

--- sent2plotly.py
+++ sent2plotly.py
@@ -1,15 +1,102 @@
 #!/usr/bin/env python
+import plotly
 import plotly.plotly as py
+
+print plotly.__version__
+
+import random
+import plotly.tools as tls
+import datetime
+import time
+import numpy as np
 from plotly.graph_objs import *
 
-trace0 = Scatter(
-    x=[1, 2, 3, 4],
-    y=[10, 15, 13, 17]
-)
-trace1 = Scatter(
-    x=[1, 2, 3, 4],
-    y=[16, 5, 11, 9]
-)
-data = Data([trace0, trace1])
+py.sign_in('tachagon', 'e3xdl3rnqe')
+stream_ids=["5oedue6mq7", "thg9gb1wwd", "p8x1frf0qx"]
+tls.set_credentials_file(stream_ids)
 
-unique_url = py.plot(data, filename = 'basic-line')
+# Get stream id from stream id list
+stream_id = stream_ids[0]
+# Make instance of stream id object
+stream = Stream(
+    token=stream_id,    # link stream id to 'token' key
+    maxpoints=80        # keep a max of 80 pts on screen
+)
+
+# Initialize trace of streaming plot by embedding the unique stream_id
+trace1 = Scatter(
+    x=[],
+    y=[],
+    mode='lines+markers',
+    stream=stream       # embed stream id, 1 per trace
+)
+
+data = Data([trace1])
+
+# Add title to layout object
+layout = Layout(title='Time Series')
+
+# Make a figure object
+fig = Figure(data=data, layout=layout)
+
+#(@) Send fig to Plotly, initialize streaming plot, open new tab
+unique_url = py.plot(fig, filename='s7_first-stream')
+
+# (@)   Make instance of the Stream link object,
+#       with same stream id as Stream id object
+s = py.Stream(stream_id)
+
+#(@) Open the stream
+s.open()
+
+i = 0   # a counter
+k = 5   # some shape parameter
+N = 200 # number of points to be plotted
+
+# Delay start of stream by 5 sec (time to switch tabs)
+time.sleep(5)
+
+while i<N:
+    i += 1  # add to counter
+    
+    # Current time on x-axis, random numbers on y-axis
+    x = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    y = (np.cos(k*i/50)*np.cos(i/50.)+np.random.randn(1))[0]
+    
+    # Both x and y are numbers (i.e. not lists nor arrays)
+    
+    # write to Plotly stream!
+    s.write(dict(x=x, y=y))
+    
+    # write number to stream to append current data on plot,
+    # write lists to overwrite existing data on plot (more in 7.2)
+    
+    time.sleep(0.08)    # plot a point every 80 ms, for smoother plotting
+    
+# Close the stream when done plotting
+s.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
