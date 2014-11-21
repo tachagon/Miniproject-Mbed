178a01bec1f5d6493296f7b9315fdb2a105a85d1
icaria555 <icaria555@hotmail.com>
2014-11-21 09:11:28 +0700

    update 2

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,5 +1,6 @@
 void setup() {
   // initialize serial communication at 9600 bits per second:
+  pinMode(4 ,OUTPUT);
   Serial.begin(9600);
 }
 
@@ -14,6 +15,8 @@ void loop() {
   Serial.print(" , temperature = ");
   Serial.println(Vtem);
   
-  Serial.write(char(Vldr) + char(Vtem) );
+  Serial.write(String(Vldr)+ " " + String(Vtem));
+  digitalWrite(4 ,HIGH);
+  
   delay(1000);        // delay in between reads for stability
 }
