14b475d9c36278054cad22b7928b9c10704dc168
icaria555 <icaria555@hotmail.com>
2014-11-29 15:40:04 +0700

    update arduino_code
    put method C language

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,6 +1,108 @@
+int LED_PIN = 3;
+#define FOSC (16000000UL)
+//*********************************** usart set and method ***********************************
+void USART_init( uint16_t baudrate ) {
+  uint16_t ubrr;
+  // UCSR0A register settings
+  // U2X0=0 => normal speed
+  // MPCM0=0 => don't use multi‐processor communication mode
+  UCSR0A = 0;
+  
+#ifdef DOUBLE_SPEED
+  UCSR0A |= (1<<U2X0); // use double speed
+  ubrr = ((FOSC/8)/baudrate)-1;
+#else
+  ubrr = ((FOSC/16)/baudrate)-1 ;
+#endif
+  // set baudrate
+  UBRR0H = (uint8_t)(ubrr>>8); // high byte
+  UBRR0L = (uint8_t)(ubrr); // low byte
+  
+  // UCSR0B register settings
+  // RXEN0=1, TXEN0=1 => Enable both RX and TX
+  // UCSZ02=0 => don't use ninth‐bit data
+  // RXCIE0=0, TXCIE0=0, UDRIE0=0 => no TX/RX interrupt
+  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+  // UCSR0C register settings
+  // UMSEL01=0, UMSEL00=0 => Asynchronous USART
+  // UPM01=0, UPM00=0 => no parity bit
+  // USBS0=0 => 1‐stop bit
+  // UCSZ01=1, UCSZ00=1 => 8‐bit data
+  UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
+}
+
+void USART_send( uint8_t data ) {
+  // Wait for empty transmit buffer (the UDRE0 bit is set.)
+  while ( !(UCSR0A & (1<<UDRE0)) ) ;
+  // Put data into buffer, sends the data
+  UDR0 = data;
+}
+
+uint8_t USART_receive( void ) {
+  // Wait for data to be received (the RXC0 bit is set.)
+  while ( !(UCSR0A & (1<<RXC0)) ) ;
+  // Get and return received data from buffer
+  return UDR0;
+}
+//*********************************** end usart set and method | ADC set and method ***********************************
+
+void ADC_init() {
+  DIDR0 = (1<<ADC5D)|(1<<ADC4D)|(1<<ADC3D)|(1<<ADC2D)|(1<<ADC1D)|(1<<ADC0D);
+  // REFS1=0, REFS0=0: use AVCC, ADLAR=0: right]alignment,
+  // MUX3..0=0000 => ADC0
+  ADMUX = (1<<REFS0);
+  ADCSRB = 0; // ADTS2=0,ADTS1=0,ADTS0=0 (with ADATE=1): free]running mode
+  // 10]bit resolution requires a clock between 50khz and 200kHz.
+  ADCSRA = (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); // set ADC clock prescaler: 128
+  // enable/start ADC, auto]trigger (ADATE=1)
+  ADCSRA |= (1<<ADEN)|(1<<ADSC)|(1<<ADATE);
+  // The first conversion starts 25 FADC cycles after ADEN set
+  // 16MHz/128 = 125kHz => 8usec x 25 = 200 usec at least
+  delayMicroseconds(205);
+}
+
+uint16_t readADC( uint8_t channel ){
+  static uint8_t current_channel = 0;
+  uint16_t x;
+  if( current_channel != channel) {
+    current_channel = channel;
+    while ( !(ADCSRA & (1<<ADIF)) );
+    ADCSRA |= (1<<ADIF);
+    ADMUX &= 0xF0;
+    ADMUX |= (channel & 0x0F);
+    delayMicroseconds(205);
+  }
+  x = (uint16_t)ADCL;
+  x |= ((uint16_t)ADCH << 8);
+  return (x & 0x3FF);
+}
+//*********************************** end ADC set and method | time/ counter 1 set and method ***********************************
+
+void T1_init() {
+  TCNT1 = 0;
+  OCR1A = 0;
+  OCR1B = 0;
+  ICR1 = 0;
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TIMSK1 = 0; // disable all interrupts
+  TIFR1 = (1<<ICF1) | (1<<OCF1B) | (1<<OCF1A) | (1<<TOV1); // clear all flags
+  // 16MHz/1024 = 15625Hz => 65536]15625 = 49911
+  TCNT1 = 49911;
+  // Use Timer/Count1 in normal mode (no output compare, no PWM)
+  TCCR1B |= (1<<CS12) | (1<<CS10); // prescaler=1024
+  TIMSK1 = (1<<TOIE1); // Timer1 Overflow Interrupt Enable
+}
+
+
+//*********************************** end time/ counter 1 set and method ***********************************
 void setup() {
   // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  pinMode(LED_PIN , OUTPUT);
+  USART_init(9600);
+  ADC_init();
+  DDRD |= (1<<DDD1); // PD1/TXD as output
+  //Serial.begin(9600);
 }
 
 void loop() {
@@ -15,6 +117,13 @@ void loop() {
   Serial.println(Vtem);
   */
   String str = String(Vldr)+","+String(Vtem)+",\n";
-  Serial.print(str);
-  delay(1000);        // delay in between reads for stability
+  //Serial.print(str);
+  digitalWrite(LED_PIN , HIGH);
+  delay(200);
+  digitalWrite(LED_PIN , LOW);
+  delay(800);        // delay in between reads for stability
 }
+
+//******************************** method ***********************************
+
+
