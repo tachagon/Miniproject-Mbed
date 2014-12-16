9bec5f0ee6689d0ebbee84f3dae59c3ecdd706ae
icaria555 <icaria555@hotmail.com>
2014-12-17 00:18:55 +0700

    arduino code version 1.00

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,6 +1,8 @@
-int LED_PIN = 3;
+
 #define FOSC (16000000UL)
+#include <avr/io.h>
 //*********************************** usart set and method ***********************************
+
 void USART_init( uint16_t baudrate ) {
   uint16_t ubrr;
   // UCSR0A register settings
@@ -36,6 +38,11 @@ void USART_send( uint8_t data ) {
   while ( !(UCSR0A & (1<<UDRE0)) ) ;
   // Put data into buffer, sends the data
   UDR0 = data;
+}
+void send_m( String &s ) {
+  for(uint16_t i = 0 ; i < s.length(); i++){
+    USART_send(s[i]);
+  }
 }
 
 uint8_t USART_receive( void ) {
@@ -76,6 +83,7 @@ uint16_t readADC( uint8_t channel ){
   x |= ((uint16_t)ADCH << 8);
   return (x & 0x3FF);
 }
+
 //*********************************** end ADC set and method | time/ counter 1 set and method ***********************************
 
 void T1_init() {
@@ -87,28 +95,43 @@ void T1_init() {
   TCCR1B = 0;
   TIMSK1 = 0; // disable all interrupts
   TIFR1 = (1<<ICF1) | (1<<OCF1B) | (1<<OCF1A) | (1<<TOV1); // clear all flags
-  // 16MHz/1024 = 15625Hz => 65536]15625 = 49911
-  TCNT1 = 49911;
+  TCNT1 = 0;
   // Use Timer/Count1 in normal mode (no output compare, no PWM)
   TCCR1B |= (1<<CS12) | (1<<CS10); // prescaler=1024
   TIMSK1 = (1<<TOIE1); // Timer1 Overflow Interrupt Enable
 }
+volatile uint16_t count = 0;
 
+ISR(TIMER1_OVF_vect) { // ISR for Timer/Counter1 Overflow Interrupt
+  count++; // increment counter
+}
+
+void delay_test(uint16_t msec){
+  TCCR1B |= (1<<CS12) | (1<<CS10); // prescaler=1024
+  count = 0;
+  TCNT1 = 0;
+  uint16_t real_count = msec/0.064;
+  uint16_t b = real_count/65535;
+  uint16_t pece = real_count%65535;
+  while(TCNT1 < pece or count < b);
+}
 
 //*********************************** end time/ counter 1 set and method ***********************************
+int LED_PIN = 3;
+
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  pinMode(LED_PIN , OUTPUT);
-  USART_init(9600);
+  DDRD |= 1 << DDD3;// set pin3 = out
+  USART_init(9600);//set rate
   ADC_init();
+  T1_init();
   DDRD |= (1<<DDD1); // PD1/TXD as output
-  //Serial.begin(9600);
+  //Serial.begin(9600); 
 }
 
 void loop() {
   // read the input on analog pin 0:
-  int Vldr = analogRead(A0);
-  int Vtem = analogRead(A1);
+  int Vldr = readADC(0);
+  int Vtem = readADC(1);
   // print out the value you read:
   /*
   Serial.print("LDR = ");
@@ -116,14 +139,20 @@ void loop() {
   Serial.print(" , temperature = ");
   Serial.println(Vtem);
   */
-  String str = String(Vldr)+","+String(Vtem)+",\n";
+  String str = String(Vldr)+","+String(Vtem)+"\n";
   //Serial.print(str);
-  digitalWrite(LED_PIN , HIGH);
-  delay(200);
-  digitalWrite(LED_PIN , LOW);
-  delay(800);        // delay in between reads for stability
+  //int b = 20;
+  //char test[] = "abc" + char(b);
+  //char da[10] = String(Vldr) + "," + String(Vtem);
+  uint8_t data = 100;
+  send_m(str);
+  PORTD |= 1 << 3;
+  delay_test(1000);
+  PORTD &= ~( 1 << 3 );
+  delay_test(1000);        // delay in between reads for stability
 }
 
 //******************************** method ***********************************
+
 
 
